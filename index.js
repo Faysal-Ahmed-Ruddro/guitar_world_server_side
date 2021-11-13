b1bfa8460eafc144c419559903a0277563744e21
Faysal-Ahmed-Ruddro <faysalahmed4184@gmail.com>
2021-11-13 22:02:35 +0600

    already done

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const ObjectId = require("mongodb").ObjectId
+const ObjectId = require("mongodb").ObjectId;
 const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 // middleware
 app.use(cors());
@@ -24,83 +22,69 @@ async function run() {
     await client.connect();
     const database = client.db("guitars_world");
     const guitarCollection = database.collection("guitars");
-    const ordersCollection = database.collection("orders")
-    const usersCollection = database.collection("users")
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
     console.log("database connected successfully");
 
-   
-    // GUITARS GET API 
-    app.get("/guitars", async(req,res)=>{
-      const cursor = guitarCollection.find({})
-      const guitars = await cursor.toArray()
-      res.json(guitars)
-    })
+    // GUITARS GET API
+    app.get("/guitars", async (req, res) => {
+      const cursor = guitarCollection.find({});
+      const guitars = await cursor.toArray();
+      res.json(guitars);
+    });
     // SINGLE GUITARS  API
-    app.get("/guitars/:id", async(req,res)=>{
+    app.get("/guitars/:id", async (req, res) => {
       console.log(req.params.id);
-        const id = req.params.id;
-        const query = {_id:ObjectId(id)}
-        const guitars = await guitarCollection.findOne(query)
-        res.json(guitars);
-        console.log(guitars);
-    })
-    app.delete("/guitars/:id",async(req,res)=>{
-        const id = req.params.id;
-        const  query = {_id:ObjectId(id)}
-        const result = await guitarCollection.deleteOne(query)
-        res.json(result)
-    })
-    
-    // Orders GET API
-    app.get("/orders",async(req,res)=>{
-      const email = req.query.email;
-      const query ={email: email} ;
-      console.log(query);
-      const cursor = ordersCollection.find(query)
-      const orders = await cursor.toArray()
-      res.json(orders)
-    })
-    //  Orders POST API
-    app.post("/orders",async(req,res)=>{
-      const orders = req.body
-      const result = await ordersCollection.insertOne(orders)
-      console.log(result);
-      res.json(result)
-    })
-    
-   // ADD GUITAR POST API
-    app.post("/guitars", async (req, res) => {
-      const guitars = req.body
-        console.log("hit the post api", guitars);
-        const result = await guitarCollection.insertOne(guitars);
-        console.log(result);
-        res.json(result)
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const guitars = await guitarCollection.findOne(query);
+      res.json(guitars);
+      console.log(guitars);
+    });
+    app.delete("/guitars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await guitarCollection.deleteOne(query);
+      res.json(result);
     });
 
-    // // USER GET API FOR MAKE ADMIN
+    // Orders GET API
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(query);
+      const cursor = ordersCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+    //  Orders POST API
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      console.log(result);
+      res.json(result);
+    });
 
-    // app.get("/users/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   const query = { email: email };
-    //   const user = await usersCollection.findOne(query);
-    //   let isAdmin = false;
-    //   if (user?.role === "admin") {
-    //     isAdmin = true;
-    //   }
-    //   res.json({ admin: isAdmin });
-    // });
+    // ADD GUITAR POST API
+    app.post("/guitars", async (req, res) => {
+      const guitars = req.body;
+      console.log("hit the post api", guitars);
+      const result = await guitarCollection.insertOne(guitars);
+      console.log(result);
+      res.json(result);
+    });
 
     // USER POST API
-   app.post("/users",async(req,res)=>{
-     const user = req.body;
-     console.log(req.body);
-     const result = await usersCollection.insertOne(user);
-  
-     res.json(result);
-   })
-  //   update user 
-  app.put("/users",async (req, res) => {
-    const user = req.body;
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(req.body);
+      const result = await usersCollection.insertOne(user);
+
+      res.json(result);
+    });
+    //   update user
+    app.put("/users", async (req, res) => {
+      const user = req.body;
       const filter = { email: user.email };
       const options = { upsert: true };
       const updateDoc = { $set: user };
@@ -110,42 +94,27 @@ async function run() {
         options
       );
       res.json(result);
-  })
-  
-    // // Update user
-    // app.put("/users", async (req, res) => {
-    //   const user = req.body;
-    //   const filter = { email: user.email };
-    //   const options = { upsert: true };
-    //   const updateDoc = { $set: user };
-    //   const result = await usersCollection.updateOne(
-    //     filter,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.json(result);
-    // });
+    });
 
-    // Modify the user
-    // app.put("/users/admin", verifyToken, async (req, res) => {
-    //   const user = req.body;
-    //   const requester = req.decodedEmail;
-    //   if (requester) {
-    //     const requesterAccount = await usersCollection.findOne({
-    //       email: requester,
-    //     });
-    //     if (requesterAccount.role === "admin") {
-    //       const filter = { email: user.email };
-    //       const updateDoc = { $set: { role: "admin" } };
-    //       const result = await usersCollection.updateOne(filter, updateDoc);
-    //       res.json(result);
-    //     }
-    //   } else {
-    //     res
-    //       .status(401)
-    //       .json({ message: "You Don't have access to make admin" });
-    //   }
-    // });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    //  makeing Admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user?.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
